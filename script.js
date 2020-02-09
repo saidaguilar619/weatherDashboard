ba056a2b51f0e8e1befc40325de438d1b74c9d4c
saidaguilar619 <58634200+saidaguilar619@users.noreply.github.com>
2020-02-08 21:09:55 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,40 @@ const apiKey = "e56a9eded7bce5b55a739696279fa85f";
 let currentCity = "San Diego";
 const currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&APPID=" + apiKey;
 const currentForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&APPID=" + apiKey;
+
+let searchArr = JSON.parse(localStorage.getItem('searchArray'));
+let searchArr2 = 
 // const currentDate = moment().format("MMMM Do, YYYY");
 $(".cbtn").on("click", function(){
     currentCity = $(this).text();
     getCurrentWeather(currentCity);
+   
 })
 $(".sbtn").on("click", function(){
     currentCity = $(".search").val();
     getCurrentWeather(currentCity);
+    let newBtn = $("<button>").addClass("btn btn-secondary cbtn").attr("type","button").text(currentCity);
+    newBtn.on("click", function(){
+        currentCity = $(this).text();
+        getCurrentWeather(currentCity);
+    })
+    $(".btn-group-vertical").append(newBtn);
+    searchArr.push(currentCity); 
+    localStorage.setItem("searchArray",JSON.stringify(searchArr));
 })
-
-
-
-
+function setSearch(searchHistory){
+    if(searchHistory){
+        $(".btn-group-vertical").append("Search History:");
+        for(let i = 0 ; i < searchHistory.length;i++){
+            let newBtn = $("<button>").addClass("btn btn-secondary cbtn").attr("type","button").text(searchHistory[i]);
+            newBtn.on("click", function(){
+                currentCity = $(this).text();
+                getCurrentWeather(currentCity);
+            })
+            $(".btn-group-vertical").append(newBtn);   
+        } 
+    }
+}
 function displayCurrentWeather(weatherData) {
     $(".city").text(weatherData.name);
     $(".temp").text("Temp: " + weatherData.main.temp);
@@ -44,4 +65,5 @@ function getUVIndex(longitude, latitude) {
         $(".UVIndex").text("UV Index: " +uvIndex);
     });
 }
+setSearch(searchArr);
 getCurrentWeather(currentCity);
