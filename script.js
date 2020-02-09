9bb836b7723a0b7cb672abfee2c4eec381a2c196
saidaguilar619 <58634200+saidaguilar619@users.noreply.github.com>
2020-02-08 22:02:24 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const apiKey = "e56a9eded7bce5b55a739696279fa85f";
 let currentCity = "San Diego";
 const currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&APPID=" + apiKey;
 const currentForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&APPID=" + apiKey;
-
 let searchArr = JSON.parse(localStorage.getItem('searchArray'));
-let searchArr2 = 
+if(searchArr == null){
+    searchArr = [];
+}
+console.log(searchArr);
 // const currentDate = moment().format("MMMM Do, YYYY");
 $(".cbtn").on("click", function(){
     currentCity = $(this).text();
@@ -20,11 +22,12 @@ $(".sbtn").on("click", function(){
         getCurrentWeather(currentCity);
     })
     $(".btn-group-vertical").append(newBtn);
+    console.log(searchArr);
     searchArr.push(currentCity); 
     localStorage.setItem("searchArray",JSON.stringify(searchArr));
 })
 function setSearch(searchHistory){
-    if(searchHistory){
+    if(searchHistory.length !== 0){
         $(".btn-group-vertical").append("Search History:");
         for(let i = 0 ; i < searchHistory.length;i++){
             let newBtn = $("<button>").addClass("btn btn-secondary cbtn").attr("type","button").text(searchHistory[i]);
@@ -65,5 +68,20 @@ function getUVIndex(longitude, latitude) {
         $(".UVIndex").text("UV Index: " +uvIndex);
     });
 }
+function getForecast(){
+    $.get(currentForecastURL)
+    .then(function(response) {  
+        for(let i = 0; i< 5;i++){
+            let temp = response[0].value;
+            let humid = response[0].value;
+            let tempString = "temp" + i;
+            let humidString = "humidity" + i;
+            // console.log(response);
+            // $(".UVIndex").text("UV Index: " +uvIndex);
+        }
+    });
+}
+
 setSearch(searchArr);
 getCurrentWeather(currentCity);
+getForecast();
