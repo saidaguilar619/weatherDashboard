1f1accf51bc8850cb82bf42ebf1e7aa94cf0cdfd
saidaguilar619 <58634200+saidaguilar619@users.noreply.github.com>
2020-02-08 22:25:17 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = "e56a9eded7bce5b55a739696279fa85f";
 let currentCity = "San Diego";
-const currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&APPID=" + apiKey;
-const currentForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&APPID=" + apiKey;
+const currentWeatherURL = "HTTPS://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&APPID=" + apiKey;
+const currentForecastURL = "HTTPS://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&APPID=" + apiKey;
 let searchArr = JSON.parse(localStorage.getItem('searchArray'));
 if(searchArr == null){
     searchArr = [];
@@ -46,7 +46,7 @@ function displayCurrentWeather(weatherData) {
     $(".windSpeed").text("Wind: " + weatherData.wind.speed + " MPH");
 }
 function getCurrentWeather(city){
-        const currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + apiKey;
+        const currentWeatherURL = "HTTPS://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + apiKey;
         $.get(currentWeatherURL)
         .then(function(response) {
              displayCurrentWeather(response);
@@ -61,27 +61,27 @@ function getCurrentWeather(city){
 function getUVIndex(longitude, latitude) {
     lon = longitude;
     lat = latitude;
-    const currentUVIURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" +apiKey+ "&lat=" + lat + "&lon=" + lon+ "&cnt=1";
+    const currentUVIURL = "HTTPS://api.openweathermap.org/data/2.5/uvi/forecast?appid=" +apiKey+ "&lat=" + lat + "&lon=" + lon+ "&cnt=1";
     $.get(currentUVIURL)
     .then(function(response) {  
         const uvIndex = response[0].value;
         $(".UVIndex").text("UV Index: " +uvIndex);
     });
 }
-function getForecast(){
-    $.get(currentForecastURL)
-    .then(function(response) {  
-        for(let i = 0; i< 5;i++){
-            let temp = response[0].value;
-            let humid = response[0].value;
-            let tempString = "temp" + i;
-            let humidString = "humidity" + i;
-            // console.log(response);
-            // $(".UVIndex").text("UV Index: " +uvIndex);
-        }
-    });
-}
+// function getForecast(){
+//     $.get(currentForecastURL)
+//     .then(function(response) {  
+//         for(let i = 0; i< 5;i++){
+//             let temp = response[0].value;
+//             let humid = response[0].value;
+//             let tempString = "temp" + i;
+//             let humidString = "humidity" + i;
+//             // console.log(response);
+//             // $(".UVIndex").text("UV Index: " +uvIndex);
+//         }
+//     });
+// }
 
 setSearch(searchArr);
 getCurrentWeather(currentCity);
-getForecast();
+// getForecast();
